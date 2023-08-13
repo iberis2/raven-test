72da2c385d0424298b2a65ba0e2042a4d99d9891
iberis2 <snow9402@gmail.com>
2023-08-13 22:02:37 +0900

    Feat: 질문하러 가기 버튼, 로그아웃 버튼 완성

---
+++ src/components/Questions/Question.tsx
@@ -0,0 +1,11 @@
+// [
+//   {
+//     "id": "1",
+//     "time": 1690677703,
+//     "latex": "$f(x)=\\sin \\left(x+\\frac{\\pi}{2}\\right)-\\cos ^{2}(x+\\pi)$"
+//   }
+// ]
+
+export default function Question() {
+  return <div>quest</div>
+}
--- src/pages/Questions.tsx
+++ src/pages/Questions.tsx
@@ -1,3 +1,30 @@
+import { useEffect } from 'react'
+import Question from '../components/Questions/Question'
+import { Link } from 'react-router-dom'
+import { removeUserIdFromLocalStorage } from '../lib/localStorage'
+import styles from './Questions.module.css'
+
 export default function Questions() {
-  return <div>questions</div>
+  useEffect(() => {
+    // GET questions
+  }, [])
+
+  const handleLogOut = () => {
+    removeUserIdFromLocalStorage()
+    location.reload()
+  }
+
+  return (
+    <div className={styles.questions}>
+      <div className={styles.btnBox}>
+        <Link to='/ask' className={styles.goAsk}>
+          질문 등록
+        </Link>
+        <button type='button' onClick={handleLogOut} className={styles.logOut}>
+          로그아웃
+        </button>
+      </div>
+      <Question />
+    </div>
+  )
 }
